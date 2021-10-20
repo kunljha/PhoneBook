e68d2275e00003605ad521b1e7b6017bd022f418
kunljha <jha711kunal@gmail.com>
2021-10-20 21:17:28 +0530

    hashing password with bcrypt

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,5 +1,6 @@
 const express = require('express')
-const { body, validationResult, check } = require('express-validator')
+const { validationResult, check } = require('express-validator')
+const bcrypt = require('bcrypt')
 const User = require('../models/User')
 
 const router = express.Router()
@@ -14,13 +15,36 @@ router.post(
 			'Please enter a password of minimun 6 or more characters'
 		).isLength({ min: 6 }),
 	],
-	(req, res) => {
+	async (req, res) => {
 		const errors = validationResult(req)
 		if (!errors.isEmpty()) {
 			return res.status(400).json({ errors: errors.array() })
 		}
 
-		res.send('passed')
+		const { name, email, password } = req.body
+		try {
+			let user = await User.findOne({ email })
+			if (user) {
+				return res
+					.status(400)
+					.json({ msg: 'User already exists with this email!' })
+			}
+			// creating instance of user
+			user = new User({
+				name,
+				email,
+				password,
+			})
+			// hashing password with bcrypt before saving in db
+			const salt = await bcrypt.genSalt()
+			user.password = await bcrypt.hash(password, salt)
+			// saving the user in db
+			const userSaved = await user.save()
+			res.status(201).json({ user: userSaved })
+		} catch (err) {
+			console.error(err.message)
+			res.status(500).send('Something went wrong!')
+		}
 	}
 )
 

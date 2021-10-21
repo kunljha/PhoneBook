6893896fc9696786bd27f6dc5e4a5810045e00a3
kunljha <jha711kunal@gmail.com>
2021-10-21 18:43:08 +0530

    register & login routes sends jwt now

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,12 +1,64 @@
 const express = require('express')
+const config = require('config')
+const { validationResult, check } = require('express-validator')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const User = require('../models/User')
+
 const router = express.Router()
 
 router.get('/', (req, res) => {
 	res.send('Get logged-in user')
 })
 
-router.post('/', (req, res) => {
-	res.send('Login user')
-})
+router.post(
+	'/',
+	[
+		check('email', 'Please enter a valid email!').isEmail(),
+		check(
+			'password',
+			'Please enter a password of minimun 6 or more characters'
+		).exists(),
+	],
+	async (req, res) => {
+		const errors = validationResult(req)
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() })
+		}
+
+		const { email, password } = req.body
+		try {
+			let user = await User.findOne({ email })
+			// if user does not exist in db
+			if (!user) {
+				return res.status(400).json({ msg: 'Email is not registered!' })
+			}
+			const isMatch = await bcrypt.compare(password, user.password)
+			//  if passworrd did not match
+			if (!isMatch) {
+				return res.status(400).json({ msg: 'Password is wrong!' })
+			}
+
+			const payload = {
+				user: {
+					id: user.id,
+				},
+			}
+
+			jwt.sign(
+				payload,
+				config.get('jwtSecret'),
+				{ expiresIn: 1 * 24 * 60 * 60 },
+				(err, token) => {
+					if (err) throw new err()
+					res.json({ token })
+				}
+			)
+		} catch (err) {
+			console.error(err.message)
+			res.status(500).send('Something went wrong!')
+		}
+	}
+)
 
 module.exports = router
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,8 @@
 const express = require('express')
+const config = require('config')
 const { validationResult, check } = require('express-validator')
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 const User = require('../models/User')
 
 const router = express.Router()
@@ -39,8 +41,23 @@ router.post(
 			const salt = await bcrypt.genSalt()
 			user.password = await bcrypt.hash(password, salt)
 			// saving the user in db
-			const userSaved = await user.save()
-			res.status(201).json({ user: userSaved })
+			await user.save()
+
+			const payload = {
+				user: {
+					id: user.id,
+				},
+			}
+
+			jwt.sign(
+				payload,
+				config.get('jwtSecret'),
+				{ expiresIn: 1 * 24 * 60 * 60 },
+				(err, token) => {
+					if (err) throw new err()
+					res.json({ token })
+				}
+			)
 		} catch (err) {
 			console.error(err.message)
 			res.status(500).send('Something went wrong!')

6a283fefa4e9cdc6aea9b2df246f6a63c15e762e
kunljha <jha711kunal@gmail.com>
2021-10-23 20:46:38 +0530

    defined add-contact request handler

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const { validationResult, check } = require('express-validator')
 const requireAuth = require('../middleware/authMiddleware')
 const Contact = require('../models/Contact')
 
@@ -16,9 +17,40 @@ router.get('/', requireAuth, async (req, res) => {
 	}
 })
 
-router.post('/', (req, res) => {
-	res.send('Add a new contact')
-})
+router.post(
+	'/',
+	[
+		requireAuth,
+		[
+			check('name', 'Please add a name of contact').not().isEmpty(),
+			check('email', 'Please add a valid email of contact').isEmail(),
+			check('phone', 'Please add phone number of contact'),
+		],
+	],
+	async (req, res) => {
+		const errors = validationResult(req)
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() })
+		}
+
+		const { name, email, phone, type } = req.body
+		try {
+			const newContact = new Contact({
+				name,
+				email,
+				phone,
+				type,
+				user: req.user.id,
+			})
+
+			const contact = await newContact.save()
+			res.json(contact)
+		} catch (err) {
+			console.error(err.message)
+			res.status(500).json({ msg: 'Something went wrong!' })
+		}
+	}
+)
 
 router.put('/:id', (req, res) => {
 	res.send('Update a contact')

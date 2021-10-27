1ecf16cf70fa1bb4c5835c3090d3e560fa42ca0d
kunljha <jha711kunal@gmail.com>
2021-10-27 23:08:40 +0530

    defined Navbar component

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import './App.css'
+import Navbar from './components/layout/Navbar'
 
 const App = () => {
 	return (
 		<div className='App'>
 			<h2>Hello from React</h2>
+			<Navbar />
 		</div>
 	)
 }

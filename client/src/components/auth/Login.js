090831f25f40a0cf017f43cd1b90bc45c8126348
kunljha <jha711kunal@gmail.com>
2021-11-05 21:17:46 +0530

    defined Register & Login component

---
+++ client/src/components/auth/Login.js
@@ -0,0 +1,57 @@
+import React, { useState } from 'react'
+
+const Login = () => {
+	const [user, setUser] = useState({
+		email: '',
+		password: '',
+	})
+
+	const handleChange = (e) => {
+		setUser({
+			...user,
+			[e.target.name]: e.target.value,
+		})
+	}
+
+	const handleSubmit = (e) => {
+		e.preventDefault()
+		console.log('Login Submit')
+	}
+
+	const { email, password } = user
+
+	return (
+		<div className='form-container'>
+			<h1>
+				Account <span className='text-primary'>Login</span>
+			</h1>
+			<form onSubmit={handleSubmit}>
+				<div className='form-group'>
+					<label htmlFor='email'>Email Address</label>
+					<input
+						type='email'
+						name='email'
+						value={email}
+						onChange={handleChange}
+					/>
+				</div>
+				<div className='form-group'>
+					<label htmlFor='password'>Password</label>
+					<input
+						type='password'
+						name='password'
+						value={password}
+						onChange={handleChange}
+					/>
+				</div>
+				<input
+					type='submit'
+					value='Login'
+					className='btn btn-primary btn-block'
+				/>
+			</form>
+		</div>
+	)
+}
+
+export default Login
